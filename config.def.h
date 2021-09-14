4cea22de8d8d20c2eecc6b01cd45c8e28e89ac3e
Timm Hirsens <timmhirsens@gmail.com>
2021-09-14 08:27:16 +0200

    gitignore & config

--- config.def.h
+++ config.def.h
@@ -65,5 +65,6 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
+	{ cpu_perc, "%s%%  ", 	    NULL    },
 	{ datetime, "%s",           "%F %T" },
 };
