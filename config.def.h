6befecd4d0be94b90cf889c5bc09faca25d358bb
Timm Hirsens <timmhirsens@gmail.com>
2021-09-20 21:28:56 +0200

    rice

--- config.def.h
+++ config.def.h
@@ -65,6 +65,7 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ cpu_perc, "%s%%  ", 	    NULL    },
+	{ cpu_perc, "[CPU: %s%%]  ",  NULL  },
+	{ ram_used, "[RAM: %s]  ",  NULL    },
 	{ datetime, "%s",           "%F %T" },
 };
