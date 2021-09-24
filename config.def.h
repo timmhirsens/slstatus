a5fe6a8e103a4d1bf02f882224d19affc744cc8a
Timm Hirsens <timmhirsens@gmail.com>
2021-09-24 18:07:12 +0200

    config

--- config.def.h
+++ config.def.h
@@ -1,9 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 
-/* interval between updates (in ms) */
-const unsigned int interval = 1000;
-
-/* text to show if no value can be retrieved */
+/* interval between updates (in ms) */ const unsigned int interval = 1000; /* text to show if no value can be retrieved */
 static const char unknown_str[] = "n/a";
 
 /* maximum output string length */
@@ -67,5 +64,9 @@ static const struct arg args[] = {
 	/* function format          argument */
 	{ cpu_perc, "[CPU: %s%%]  ",  NULL  },
 	{ ram_used, "[RAM: %s]  ",  NULL    },
+	{ battery_perc, "[BAT: %s%%",  "BAT0" },
+	{ battery_state, "%s]  ",  "BAT0" },
+    { wifi_essid, "[%s: ", "wlp2s0" },
+    { wifi_perc, "%s%%] ", "wlp2s0" },
 	{ datetime, "%s",           "%F %T" },
 };
